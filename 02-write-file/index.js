4bf7f7c27deae949a434a3c9fe1be4941175e7a6
Irene Strigo <irene.strigo@gmail.com>
2024-01-17 15:21:45 +0100

    feat: second task

---
+++ 02-write-file/index.js
@@ -0,0 +1,32 @@
+const fs = require("fs");
+const path = require("path");
+const readline = require("readline");
+const process = require("process");
+const {
+    stdin: input,
+    stdout: output,
+} = require("process");
+
+const rl = readline.createInterface({ input, output });
+
+const writeableStream = fs.createWriteStream(path.posix.basename("02-write-file\\greeting.txt"));
+writeableStream.write("Введите текст:");
+console.log('Введите текст:')
+rl.on('line', (input) => {
+    if (input != '') {
+        console.log(`Получено: ${input}`);
+        console.log('Введите текст и нажмите Enter:');
+        writeableStream.write(input);
+    }
+    if (input.toLowerCase() === 'exit') {
+        rl.close();
+    }
+});
+
+rl.on('close', () => {
+    fs.unlink(path.posix.basename("02-write-file\\greeting.txt"), err => {
+        if (err) throw err;
+        console.log('Файл успешно удалён');
+    })
+});
+
